6a0afb39361b42bffc3aee017f29c0840b53a93c
daniel3scripts <paul.lugo@tecsup.edu.pe>
2022-04-11 03:25:42 -0500

    layouts

---
+++ App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio01Layout
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            //MainPage = new StackLayoutDemo();
+            //MainPage = new GridDemo();
+            MainPage = new StacklayoutCode();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ StacklayoutCode.xaml.cs
@@ -0,0 +1,55 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio01Layout
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StacklayoutCode : ContentPage
+    {
+        public StacklayoutCode ()
+        {
+            InitializeComponent ();
+            var layout = new StackLayout();
+            var button = new Button
+            {
+                Text = "StackLayout - Qatar 2022 - Lugo",
+                VerticalOptions = LayoutOptions.Start,
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+            var yellowBox = new BoxView
+            {
+                Color = Color.Yellow,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+
+            var greenBox = new BoxView
+            {
+                Color = Color.Green,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+
+            var blueBox = new BoxView
+            {
+                Color = Color.Blue,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                HeightRequest = 75
+            };
+
+            layout.Children.Add(button);
+            layout.Children.Add(yellowBox);
+            layout.Children.Add(greenBox);
+            layout.Children.Add(blueBox);
+            layout.Spacing = 30;
+            Content = layout;
+        }
+    }
+}
